6cb2369f1e4ac59e948108c825df7b9f9b20339c
mattthewaz <mattthewaz@gmail.com>
2015-06-08 18:21:15 -0400

    Added point class, and api documentation pertaining to it

---
+++ Scripts/point.js
@@ -0,0 +1,22 @@
+﻿HF.point = function(q, r, s)
+{
+    return 
+    {
+        q = q,
+        r = r,
+        s = s,
+
+        add = function (q, r, s) {
+            return HF.point(this.q + q, this.r + r, this.s + s);
+        },
+
+        addPoint = function (otherPoint) {
+            return this.add(otherPoint.q, otherPoint.r, otherPoint.s);
+        },
+
+        invert = function ()
+        {
+            return HF.point(-this.q, -this.r, -this.s);
+        }
+    };
+};
