60bc1414d11b524be67c110bc89a959f3e35bf51
mattthewaz <mattthewaz@gmail.com>
2015-06-09 13:53:20 -0400

    Simplified HF.vector.add()

--- HF/vector.js
+++ HF/vector.js
@@ -14,37 +14,10 @@ HF.vector = function (direction, magnitude)
 
             var combinedPath = thisPath.add(otherPath);
             
-            //Here's the fun part. Find the unit vector and the magnitude. Not too hard.
-            //Step 1: Add up the positive and negative components separately
-            var positiveMagnitude = 0;
-            var negativeMagnitude = 0;
-            var positiveComponents = 0;
-            var negativeComponents = 0;
-            
-            var checkComponent = function (value)
-            {
-                if (value >= 0) {
-                    positiveMagnitude += value;
-                    positiveComponents++;
-                }
-                else {
-                    negativeMagnitude += value;
-                    negativeComponents++;
-                }
-            }
-
-            checkComponent(combinedPath.q);
-            checkComponent(combinedPath.r);
-            checkComponent(combinedPath.s);
-
-            //Use magnitude from whichever directionality (positive or negative) has more components
-            var newMagnitude = positiveComponents > negativeComponents ? positiveMagnitude : -negativeMagnitude;
-
-            //Step 2: Divide combined path by new magnitude
-            //If magnitude is 0, use zero direction
+            var newMagnitude = (Math.abs(combinedPath.q) + Math.abs(combinedPath.r) + Math.abs(combinedPath.s))/2;
             var newDirection = newMagnitude != 0 ? combinedPath.scale(1 / newMagnitude) : HF.hexPoint(0, 0, 0);
 
-            //Step 3: Ensure q + r + s = 0
+            //Ensure q + r + s = 0
             newDirection = newDirection.validate();
 
             return HF.vector(newDirection, newMagnitude);
