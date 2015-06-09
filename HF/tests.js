ae82f4b8fad8990dd02b7746fb19ec66eae6cbfc
mattthewaz <mattthewaz@gmail.com>
2015-06-09 13:47:12 -0400

    Ignore applicationhost.config... for now

---
+++ HF/tests.js
@@ -0,0 +1,15 @@
+﻿//HF Namespace
+HF = this.HF != undefined ? this.HF : {};
+
+HF.tests = function ()
+{
+    var p1 = HF.hexPoint(1, -1, 0);
+    var p2 = HF.directions.unit['r'];
+    var v1 = HF.vector(p1, 10);
+    var v2 = HF.vector(p2, 5);
+    var v3 = v1.add(v2);
+    Debug.writeln("q: " + v3.direction.q);
+    Debug.writeln("r: " + v3.direction.r);
+    Debug.writeln("s: " + v3.direction.s);
+    Debug.writeln("magnitude: " + v3.magnitude);
+}
