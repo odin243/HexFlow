1eb3d0118573826d340c394e8eda71c5811520aa
mattthewaz <mattthewaz@gmail.com>
2015-06-10 23:22:36 -0400

    Created HF.hexTile

---
+++ HF/hexTile.js
@@ -0,0 +1,28 @@
+﻿//HF Namespace
+HF = this.HF != undefined ? this.HF : {};
+
+HF.hexTile = function (location)
+{
+    if (location == undefined)
+    {
+        console.error("HF.hexTile - invalid argument - returning null");
+        return null;
+    }
+
+    return {
+        location: location,
+        power: 0,
+        flow: HF.vector(),
+        owner: null,
+
+        getIdentifier: function()
+        {
+            return location.toString();
+        },
+
+        calcEffectOnNeighbors: function ()
+        {
+
+        }
+    };
+}
