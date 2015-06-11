6ff1f9e06e18b15cc31d9c92dcb366f8b3e8d175
Tom Tharp <odin243@gmail.com>
2015-06-11 17:42:24 -0400

    Added config file for game-wide configuration values

---
+++ HF/config.js
@@ -0,0 +1,10 @@
+﻿//HF Namespace
+window.HF = window.HF || {};
+
+//This is a static class that holds game configuration values
+HF.config = function() {
+    return {
+        //This is the percentage of flow that will disperse to each of the two adjacent directions
+        flowDisperseRate: 0.15435
+    };
+}();
