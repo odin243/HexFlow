b63ff2fb9d85164643908762c26f745c7d07a1fe
Tom Tharp <odin243@gmail.com>
2015-06-11 18:18:39 -0400

    Added back in more unit tests from the reference folder

--- HF/tests.js
+++ HF/tests.js
@@ -5,15 +5,82 @@ if (!window.hasOwnProperty("Debug"))
     window.Debug = { writeln: function() {} };
 
 //Singleton
-HF.tests = function () {
+HF.tests = function() {
     return {
-        runAllTests: function () {
-            this.vectorAdditionTest();
+        runAllTests: function() {
             this.defaultHexPointTest();
+            this.hexPointArithmeticTest();
+            this.directionTest();
             this.defaultVectorTest();
+            this.vectorAdditionTest();
         },
 
-        vectorAdditionTest: function () {
+        complain: function(name) {
+            console.log("FAIL", name);
+        },
+
+        hexPointEqual: function(name, a, b) {
+            if (!(a.q === b.q && a.s === b.s && a.r === b.r))
+            {
+                this.complain(name);
+            }
+        },
+
+        intEqual: function(name, a, b) {
+            if (!(a === b))
+            {
+                this.complain(name);
+            }
+        },
+
+        hexPointArrayEqual: function(name, a, b) {
+            this.intEqual(name, a.length, b.length);
+            for (var i = 0; i < a.length; i++)
+            {
+                this.hexPointEqual(name, a[i], b[i]);
+            }
+        },
+
+        defaultHexPointTest: function() {
+            Debug.writeln("defaultHexPointTest");
+            var p1 = HF.hexPoint();
+            Debug.writeln("q: " + p1.q);
+            Debug.writeln("r: " + p1.r);
+            Debug.writeln("s: " + p1.s);
+
+            if (p1.q !== 0 || p1.r !== 0 || p1.s !== 0)
+            {
+                this.complain("defaultHexPointTest - Expected all coordinates to be 0");
+            }
+
+            Debug.writeln("");
+        },
+
+        hexPointArithmeticTest: function() {
+            this.hexPointEqual("hex_add", HF.hexPoint(4, -10, 6), HF.hexPoint(1, -3, 2).add(HF.hexPoint(3, -7, 4)));
+            this.hexPointEqual("hex_subtract", HF.hexPoint(-2, 4, -2), HF.hexPoint(1, -3, 2).subtract(HF.hexPoint(3, -7, 4)));
+        },
+
+        directionTest: function() {
+            this.hexPointEqual("hex_direction", HF.hexPoint(0, -1, 1), HF.directions.faceByIndex(2));
+        },
+
+
+        defaultVectorTest: function() {
+            Debug.writeln("defaultVectorTest");
+            var v1 = HF.vector();
+            Debug.writeln("q: " + v1.direction.q);
+            Debug.writeln("r: " + v1.direction.r);
+            Debug.writeln("s: " + v1.direction.s);
+            Debug.writeln("magnitude: " + v1.magnitude);
+
+            this.hexPointEqual("defaultVectorTest - all coordinates should be 0", v1.direction, HF.hexPoint(0, 0, 0));
+            this.intEqual("defaultVectorText - magnitude should be 0", v1.magnitude, 0);
+
+            Debug.writeln("");
+        },
+
+        vectorAdditionTest: function() {
 
             Debug.writeln("vectorAdditionTest");
             var p1 = HF.hexPoint(1, -1, 0);
@@ -25,35 +92,6 @@ HF.tests = function () {
             Debug.writeln("r: " + v3.direction.r);
             Debug.writeln("s: " + v3.direction.s);
             Debug.writeln("magnitude: " + v3.magnitude);
-
-            Debug.writeln("");
-        },
-
-        defaultHexPointTest: function () {
-            Debug.writeln("defaultHexPointTest");
-            var p1 = HF.hexPoint();
-            Debug.writeln("q: " + p1.q);
-            Debug.writeln("r: " + p1.r);
-            Debug.writeln("s: " + p1.s);
-
-            if (p1.q !== 0 || p1.r !== 0 || p1.s !== 0) {
-                console.error("defaultHexPointTest - Expected all coordinates to be 0");
-            }
-
-            Debug.writeln("");
-        },
-
-        defaultVectorTest: function () {
-            Debug.writeln("defaultVectorTest");
-            var v1 = HF.vector();
-            Debug.writeln("q: " + v1.direction.q);
-            Debug.writeln("r: " + v1.direction.r);
-            Debug.writeln("s: " + v1.direction.s);
-            Debug.writeln("magnitude: " + v1.magnitude);
-
-            if (v1.direction.q !== 0 || v1.direction.r !== 0 || v1.direction.s !== 0 || v1.magnitude !== 0) {
-                console.error("defaultVectorTest - Expected all direction coordinates and magnitude to be 0");
-            }
 
             Debug.writeln("");
         }
