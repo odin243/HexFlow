e6ac61d329849dad611002389f56d4807074256c
mattthewaz <mattthewaz@gmail.com>
2015-06-10 23:20:04 -0400

    Changes to Vector
    Added default behavior to HF.vector() when calling with no arguments
    Added logic to verify parameters

--- HF/vector.js
+++ HF/vector.js
@@ -3,12 +3,23 @@ HF = this.HF != undefined ? this.HF : {};
 
 HF.vector = function (direction, magnitude)
 {
+    if (magnitude != undefined && isNaN(magnitude))
+    {
+        console.error("HF.vector - invalid argument - returning null");
+        return null;
+    }
+
     return {
-        direction: direction,
-        magnitude: magnitude,
+        direction: direction || HF.hexPoint(),
+        magnitude: magnitude || 0,
 
         add: function (otherVector)
         {
+            if (otherVector == undefined) {
+                console.error("HF.vector.add - invalid argument - returning null");
+                return null;
+            }
+
             var thisPath = this.direction.scale(this.magnitude);
             var otherPath = otherVector.direction.scale(otherVector.magnitude);
 
@@ -21,6 +32,6 @@ HF.vector = function (direction, magnitude)
             newDirection = newDirection.validate();
 
             return HF.vector(newDirection, newMagnitude);
-        }        
+        }
     };
 }
