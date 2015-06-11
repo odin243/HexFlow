4fc578b08215e55064931cc1518410b0676fc1d2
mattthewaz <mattthewaz@gmail.com>
2015-06-10 23:22:14 -0400

    Added tests for default HF.hexPoint and HF.vector

--- HF/tests.js
+++ HF/tests.js
@@ -6,9 +6,12 @@ HF.tests = function () {
     return {
         runAllTests: function () {
             this.vectorAdditionTest();
+            this.defaultHexPointTest();
+            this.defaultVectorTest();
         },
 
         vectorAdditionTest: function () {
+            Debug.writeln("vectorAdditionTest");
             var p1 = HF.hexPoint(1, -1, 0);
             var p2 = HF.directions.unit['r'];
             var v1 = HF.vector(p1, 10);
@@ -18,6 +21,37 @@ HF.tests = function () {
             Debug.writeln("r: " + v3.direction.r);
             Debug.writeln("s: " + v3.direction.s);
             Debug.writeln("magnitude: " + v3.magnitude);
+
+            Debug.writeln("");
+        },
+
+        defaultHexPointTest: function () {
+            Debug.writeln("defaultHexPointTest");
+            var p1 = HF.hexPoint();
+            Debug.writeln("q: " + p1.q);
+            Debug.writeln("r: " + p1.r);
+            Debug.writeln("s: " + p1.s);
+
+            if (p1.q != 0 || p1.r != 0 || p1.s != 0) {
+                console.error("defaultHexPointTest - Expected all coordinates to be 0");
+            }
+
+            Debug.writeln("");
+        },
+
+        defaultVectorTest: function () {
+            Debug.writeln("defaultVectorTest");
+            var v1 = HF.vector();
+            Debug.writeln("q: " + v1.direction.q);
+            Debug.writeln("r: " + v1.direction.r);
+            Debug.writeln("s: " + v1.direction.s);
+            Debug.writeln("magnitude: " + v1.magnitude);
+
+            if (v1.direction.q != 0 || v1.direction.r != 0 || v1.direction.s != 0 || v1.magnitude != 0) {
+                console.error("defaultVectorTest - Expected all direction coordinates and magnitude to be 0");
+            }
+
+            Debug.writeln("");
         }
     };
 }();
