b0e35c1b3d41b8ee89e77472db3ce1771a65de92
Tom Tharp <odin243@gmail.com>
2015-06-18 17:51:18 -0400

    Initial commit of visual classes point and scene. Added code to calculate hexagon corner points and the svg shape string.

---
+++ HF/visual/point.js
@@ -0,0 +1,65 @@
+﻿//HF.visual namespaces
+window.HF = window.HF || {};
+window.HF.visual = window.HF.visual || {};
+
+HF.visual.point = function(x, y)
+{
+    return {
+        x: x || 0,
+        y: y || 0,
+        equals: function(otherPoint)
+        {
+            return this.x === otherPoint.x && this.y === otherPoint.y;
+        },
+        toString: function()
+        {
+            return this.x + ',' + this.y;
+        },
+        lengthSquared: function()
+        {
+            return this.x * this.x + this.y * this.y;
+        },
+        length: function()
+        {
+            return Math.sqrt(this.lengthSquared());
+        },
+        normalize: function()
+        {
+            var d = this.length();
+            return HF.visual.point(this.x / d, this.y / d);
+        },
+        scale: function(d)
+        {
+            return HF.visual.point(this.x * d, this.y * d);
+        },
+        rotateLeft: function()
+        {
+            return HF.visual.point(this.y, -this.x);
+        },
+        rotateRight: function()
+        {
+            return HF.visual.point(-this.y, this.x);
+        },
+        add: function(otherPoint)
+        {
+            return HF.visual.point(this.x + otherPoint.x, this.y + otherPoint.y);
+        },
+        subtract: function(otherPoint)
+        {
+            return HF.visual.point(this.x - otherPoint.x, this.y - otherPoint.y);
+        },
+        dot: function(otherPoint)
+        {
+            return this.x * otherPoint.x + this.y * otherPoint.y;
+        },
+        cross: function(otherPoint)
+        {
+            return this.x * otherPoint.y - this.y * otherPoint.x;
+        },
+        distance: function(otherPoint)
+        {
+            return this.subtract(otherPoint).length();
+        }
+    };
+};
+
---
+++ HF/visual/scene.js
@@ -0,0 +1,33 @@
+﻿//HF.visual namespaces
+window.HF = window.HF || {};
+window.HF.visual = window.HF.visual || {};
+
+HF.visual.scene = function()
+{
+    return {
+        // center is a point object
+        // scale should be the distance from corner to corner
+        getHexCorners: function(center, scale)
+        {
+            var points = [];
+            for (var i = 0; i < 6; i++)
+            {
+                var angle = 2 * Math.PI * (2 * i) / 12;
+                points.push(HF.visual.point(
+                    center.x + 0.5 * scale * Math.cos(angle),
+                    center.y + 0.5 * scale * Math.sin(angle)));
+            }
+            return points;
+        },
+
+        makeHexShapeString: function(scale)
+        {
+            return this.getHexCorners(HF.visual.point(0, 0), scale)
+                .map(function(point) {
+                    return point.x.toFixed(3) + ',' + point.y.toFixed(3);
+                })
+                .join(' ');
+        }
+
+    };
+};
