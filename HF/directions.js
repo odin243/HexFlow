7bd758fb47831ea6177026ffd170f79316db8c9f
Tom Tharp <odin243@gmail.com>
2015-06-23 14:26:32 -0400

    Re-factored axis directions to use the same indices as the hex corner rendering logic

--- HF/directions.js
+++ HF/directions.js
@@ -3,30 +3,57 @@ window.HF = window.HF || {};
 
 HF.directions = {
     faceDirections: [
-        HF.hexPoint( 1, -1,  0),
-        HF.hexPoint( 1,  0, -1),
-        HF.hexPoint( 0,  1, -1),
-        HF.hexPoint(-1,  1,  0),
-        HF.hexPoint(-1,  0,  1),
-        HF.hexPoint( 0, -1,  1)
+        HF.hexPoint(1, -1, 0),
+        HF.hexPoint(0, -1, 1),
+        HF.hexPoint(-1, 0, 1),
+        HF.hexPoint(-1, 1, 0),
+        HF.hexPoint(0, 1, -1),
+        HF.hexPoint(1, 0, -1)
     ],
 
     indexToFace: [
         'r',
-        'ur',
-        'ul',
-        'l',
+        'lr',
         'll',
-        'lr'
+        'l',
+        'ul',
+        'ur'
     ],
 
     faceToIndex: {
-        'r' : 0,
-        'ur': 1,
-        'ul': 2,
-        'l' : 3,
-        'll': 4,
-        'lr': 5
+        'r':  0,
+        'lr': 1,
+        'll': 2,
+        'l':  3,
+        'ul': 4,
+        'ur': 5
+    },
+
+    axisDirections: [
+        HF.hexPoint(.5, -1, .5),
+        HF.hexPoint(-.5, -.5, 1),
+        HF.hexPoint(-1, .5, .5),
+        HF.hexPoint(-.5, 1, -.5),
+        HF.hexPoint(.5, .5, -1),
+        HF.hexPoint(1, -.5, -.5)
+    ],
+
+    indexToAxis: [
+        '-r',
+        's',
+        '-q',
+        'r',
+        '-s',
+        'q'
+    ],
+
+    axisToIndex: {
+        '-r': 0,
+        's':  1,
+        '-q': 2,
+        'r':  3,
+        '-s': 4,
+        'q':  5
     },
 
     face: function(faceString)
@@ -39,13 +66,22 @@ HF.directions = {
         return this.faceDirections[faceIndex];
     },
 
-    axis:
+    axis: function(axisString)
     {
-        'q': HF.hexPoint(1, -0.5, -0.5),
-        'r': HF.hexPoint(-0.5, 1, -0.5),
-        's': HF.hexPoint(-0.5, -0.5, 1),
-        'qr': HF.hexPoint(0.5, 0.5, -1),
-        'qs': HF.hexPoint(0.5, -1, 0.5),
-        'rs': HF.hexPoint(-1, 0.5, 0.5)
+        return this.axisDirections[this.axisToIndex[axisString]];
+    },
+
+    axisByIndex: function(axisIndex)
+    {
+        return this.axisDirections[axisIndex];
+    },
+
+    get: function(directionString)
+    {
+        return this.axisToIndex.hasOwnProperty(directionString)
+            ? this.axis(directionString)
+            : this.faceToIndex.hasOwnProperty(directionString)
+            ? this.face(directionString)
+            : null;
     }
 };
--- HF/tests.js
+++ HF/tests.js
@@ -97,7 +97,7 @@ HF.tests = function()
         {
             Debug.writeln('vectorAdditionTest');
             var p1 = HF.hexPoint(1, -1, 0);
-            var p2 = HF.directions.axis['r'];
+            var p2 = HF.directions.axis('r');
             var v1 = HF.vector(p1, 10);
             var v2 = HF.vector(p2, 5);
             var v3 = v1.add(v2);
