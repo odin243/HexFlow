de830cb6095a905a215064369946244f8d97b7e2
mattthewaz <mattthewaz@gmail.com>
2015-07-12 17:18:10 -0400

    Added level manager.

---
+++ HF/levelManager.js
@@ -0,0 +1,34 @@
+﻿//HF Namespace
+window.HF = window.HF || {};
+
+HF.levelManager =
+{
+    levels: {},
+    registerLevel: function (name, level)
+    {
+        this.levels = this.levels || {};
+
+        this.levels[name] = level;
+    },
+
+    loadLevel: function (name)
+    {
+        if (this.levels.hasOwnProperty(name))
+            HF.state.initialize(this.levels[name]);
+    }
+};
+
+HF.levels = function ()
+{
+    var player1 = new HF.hexTile(new HF.hexPoint(-10, 0, 10), 1000, new HF.vector(), '#FF0000', 100);
+    var player2 = new HF.hexTile(new HF.hexPoint(0, 10, -10), 1000, new HF.vector(), '#0000FF', 100);
+    var player3 = new HF.hexTile(new HF.hexPoint(10, -10, 0), 1000, new HF.vector(), '#00FF00', 100);
+    var neutralSource = new HF.hexTile(new HF.hexPoint(), 0, new HF.vector(), null, 300);
+    var map = HF.hexMapGenerator.radius(10);
+    var level = HF.level(map, [player1, player2, player3, neutralSource]);
+
+    HF.levelManager.registerLevel('test-3-corners', level);
+
+    var level2 = HF.level(HF.hexMapGenerator.radius(10), [new HF.hexTile(new HF.hexPoint(0, 0, 0), 1, new HF.vector(HF.directions.face('r'), 1), '#FF0000', 1)]);
+    HF.levelManager.registerLevel('test-dispersion', level2);
+}();
--- HF/tests.js
+++ HF/tests.js
@@ -155,19 +155,7 @@ HF.tests = function()
                 Debug.writeln('Creating map of radius 3');
             }
 
-            //Initialization
-            var player1 = new HF.hexTile(new HF.hexPoint(-10, 0, 10), 1000, new HF.vector(), '#FF0000', 100);
-            var player2 = new HF.hexTile(new HF.hexPoint(0, 10, -10), 1000, new HF.vector(), '#0000FF', 100);
-            var player3 = new HF.hexTile(new HF.hexPoint(10, -10, 0), 1000, new HF.vector(), '#00FF00', 100);
-            var neutralSource = new HF.hexTile(new HF.hexPoint(), 0, new HF.vector(), null, 300);
-            var map = HF.hexMapGenerator.radius(10);
-            var level = HF.level(map, [player1, player2, player3, neutralSource]);
-
-            var level2 = HF.level(HF.hexMapGenerator.radius(10),
-            [new HF.hexTile(new HF.hexPoint(0, 0, 0), 1, new HF.vector(HF.directions.face('r'), 1), '#FF0000', 1)]);//,
-                //new HF.hexTile(new HF.hexPoint(7,-11,4), 1, new HF.vector(HF.directions.face('r'), 1), '#FF00FF', 1)]);
-
-            HF.state.initialize(level2);
+            HF.levelManager.loadLevel('test-dispersion');
 
             if (HF.config.debug === true)
             {
